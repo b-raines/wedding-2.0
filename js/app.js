28954c8acaa2d0774adcce6fc0153bbebc12bc97
Brent Raines <bt.raines@gmail.com>
2014-02-24 11:49:53 -0500

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,36 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(document).ready(function() {
+
+	// Parallax scroll
+	$('.parallax').each(function(){
+		var $bgobj = $(this);
+		$(window).scroll(function() {
+			var yPos = -( ( $(window).scrollTop() - $bgobj.offset().top) / $bgobj.data('speed')); 
+			var coords = '50% '+ yPos + 'px';
+			$bgobj.css({ backgroundPosition: coords });
+		}); 
+	});
+
+	// Sticky
+	var stickyNavTop = $('.magellan-container').offset().top;
+
+	var stickyNav = function() {
+		var scrollTop = $(window).scrollTop();
+
+		if (scrollTop > stickyNavTop) {
+			$('.magellan-container').addClass('sticky');
+		} else {
+			$('.magellan-container').removeClass('sticky');
+		}
+	};
+
+	stickyNav();
+
+	$(window).scroll(function() {
+		stickyNav();
+	});
+
+});
