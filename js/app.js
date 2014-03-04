1243088657669be9eeaefeb355b5b00c4d1b7928
Brent Raines <bt.raines@gmail.com>
2014-03-04 12:22:41 -0500

    I haven't commited this in fucking forever.
    I did a bunch of shit, and I'm almost done.
    
    * Wrote some js for cool animations.

--- js/app.js
+++ js/app.js
@@ -14,23 +14,114 @@ $(document).ready(function() {
 		}); 
 	});
 
-	// Sticky
-	var stickyNavTop = $('.magellan-container').offset().top;
+	// set sticky nav container offset
+	var navHeight = $('.magellan-container').outerHeight() - 10;
+	var stickyNavTop = $('.first').outerHeight() - navHeight - 10;
 
-	var stickyNav = function() {
+	var stickyNav = function(navTop) {
+		// set scrolltop position
 		var scrollTop = $(window).scrollTop();
 
-		if (scrollTop > stickyNavTop) {
+		// add sticky class to logo
+		if (scrollTop > navHeight) {
+			$('.logo').addClass('sticky-logo');
+		} else {
+			$('.logo').removeClass('sticky-logo');
+		}
+
+		// add sticky class if top of nav container hits top of window
+		if (scrollTop > navTop) {
 			$('.magellan-container').addClass('sticky');
 		} else {
 			$('.magellan-container').removeClass('sticky');
 		}
-	};
+	}
 
-	stickyNav();
 
+	var maidsClick = function() {
+		$('#maids-pics').click(function(event) {
+			event.preventDefault();
+			$('.maids-pics').show();
+			$('.party').animate({
+				left: '100%'
+			}, function() {
+				$('.maids-pics').find('button').show();
+			});
+		});
+	}
+
+	var maidsHide = function() {
+		$('#maids-hide').click(function(event) {
+			event.preventDefault();
+			$('.maids-pics').find('button').hide();
+			$('.party').animate({
+				left: '0'
+			}, function() {
+				$('.maids-pics').hide();
+			});
+		});
+	}
+
+	var menClick = function() {
+		$('#men-pics').click(function(event) {
+			event.preventDefault();
+			$('.men-pics').show();
+			$('.party').animate({
+				left: '-100%'
+			}, function() {
+				$('.men-pics').find('button').show();
+			});
+		});
+	}
+
+	var menHide = function() {
+		$('#men-hide').click(function(event) {
+			event.preventDefault();
+			$('.men-pics').find('button').hide();
+			$('.party').animate({
+				left: '0'
+			}, function() {
+				$('.men-pics').hide();
+			});
+		});
+	}
+
+	maidsClick();
+	maidsHide();
+	menClick();
+	menHide();
+
+	var maidsHover = function() {
+		$('.maids').hover(function() {
+			$(this).find('h2').addClass('party-hover');
+		}, function() {
+			$(this).find('h2').removeClass('party-hover');
+		});
+	}
+
+	var menHover = function() {
+		$('.men').hover(function() {
+			$(this).addClass('men-hover');
+		}, function() {
+			$(this).removeClass('men-hover');
+		});
+	}
+
+	maidsHover();
+	menHover();
+
+	// init stickynav
+	stickyNav(stickyNavTop);
+
+	// reset nav container offset on browser resize
+	$(window).resize(function() {
+		stickyNavTop = $('.first').outerHeight() - $('.magellan-container').outerHeight();
+		stickyNav(stickyNavTop);
+	});
+
+	// reset scrolltop on scroll
 	$(window).scroll(function() {
-		stickyNav();
+		stickyNav(stickyNavTop);
 	});
 
 });
